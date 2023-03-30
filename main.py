e0ebcedcb6675ac57a5cb41e8af4b854eebc6051
jonathanm-12 <mikalovjonathan@gmail.com>
2023-03-30 16:07:20 -0400

    gui

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Chat:
         self.User2Socket = socket(AF_INET, SOCK_STREAM)
         self.User1Socket = socket(AF_INET, SOCK_STREAM)
         self.username = "Alex"
-        self.exit_event = threading.Event()
+        self.GlobalFlag = False
 
     def receiving(self):
         self.User1Socket.bind((self.User1_IP_addr, self.Port))
@@ -21,7 +21,7 @@ class Chat:
         print("Connected to", addr, "\n>")
 
         # Start receiving messages
-        while not self.exit_event.is_set():
+        while not self.GlobalFlag:
             try:
                 message = connectionSocket.recv(1024)
                 received = message.decode()
@@ -29,32 +29,33 @@ class Chat:
 
                 if content_received == "Goodbye":
                     print("Closing connection socket.")
-                    self.exit_event.set()
+                    self.GlobalFlag = True
                     connectionSocket.close()
+                    raise OSError("Goodbye")
                     return
                 elif message:
                     print("[" + time_received + "] " + username_received + " > " + content_received + "\n>")
             except error:
                 print("User has left")
                 connectionSocket.close()
-                self.exit_event.set()
+                self.GlobalFlag = True
                 return
 
     def sending(self):
         self.User2Socket.connect((self.User2_IP_addr, self.Port))
         # Send message
         try: 
-            while not self.exit_event.is_set():
+            while not self.GlobalFlag:
                 message = input("> ")
                 self.User2Socket.send(str(datetime.datetime.now().strftime("%H:%M:%S") + "#<>}" + self.username + "#<>}" + message).encode())
                 if (message == "Goodbye"):
                     self.User2Socket.close()
-                    self.exit_event.set()
+                    self.GlobalFlag = True
                     print("Connection Closed")
+                    raise OSError("Goodbye")
                     return
         except:
             print("User has left.")
-            self.exit_event.set()
             return
                 
 
@@ -69,5 +70,9 @@ class Chat:
         send.join()
         
 if __name__ == "__main__":
-    chat = Chat()
-    chat.start_chat()
+    try:
+        chat = Chat()
+        chat.start_chat()
+    except:
+        print("ok goodbye")
+        exit()
