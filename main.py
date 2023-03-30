e185f5d6c7c44cb515c2886d192b7b2228faf28d
AHP-23 <alexchp@bu.edu>
2023-03-30 13:48:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from socket import *
 import threading
 import datetime
+from socket import *
 
 Port = 2909
 User1_IP_addr = gethostbyname_ex(gethostname())[2][-1]
@@ -8,6 +8,9 @@ print("Your IP address is: ", User1_IP_addr)
 User2Socket = socket(AF_INET, SOCK_STREAM)
 User1Socket = socket(AF_INET, SOCK_STREAM)
 username = "Alex"
+
+# Flag to indicate when the sending thread should terminate
+terminate_sending = False
 
 def receiving():
     User1Socket.bind((User1_IP_addr, Port))
@@ -17,8 +20,8 @@ def receiving():
     connectionSocket, addr = User1Socket.accept()
     print("Connected to", addr, "\n>")
 
-# Start receiving messages
-    while True:
+    # Start receiving messages
+    while not terminate_sending:
         try:
             message = connectionSocket.recv(1024)
             received = message.decode()
@@ -27,40 +30,41 @@ def receiving():
             if content_received == "Goodbye":
                 print("Closing connection socket.")
                 connectionSocket.close()
-                exit()
+                # Set flag to terminate sending thread
+                global terminate_sending
+                terminate_sending = True
+                break
             elif message:
                 print("[" + time_received + "] " + username_received + " > " + content_received + "\n>")
         except error:
             print("User has left")
             connectionSocket.close()
-            exit()
+            # Set flag to terminate sending thread
+            terminate_sending = True
+            break
 
 
 def sending():
+    global terminate_sending
     User2Socket.connect((User2_IP_addr, Port))
     # Send message
-    while True:
+    while not terminate_sending:
         message = input("> ")
         User2Socket.send(str(datetime.datetime.now().strftime("%H:%M:%S") + "#<>}" + username + "#<>}" + message).encode())
         if (message == "Goodbye"):
             User2Socket.close()
             print("Connection Closed")
+            # Set flag to terminate sending thread
+            terminate_sending = True
             break
-    exit()
-
-
-
-
 
 if __name__ == "__main__":
     #User2_IP_addr = input("Enter IP address of User2: ")
     User2_IP_addr = "192.168.8.239"
-    rcv = threading.Thread(target=receiving, name="rcv").start()
-    send = threading.Thread(target=sending, name="send").start()
-
-    while True:
-        if rcv.join(0) is None and send.join(0) is None:
-            continue
-        else:
-            print("Either receiving or sending thread has finished")
-            exit()
+    rcv = threading.Thread(target=receiving, name="rcv")
+    send = threading.Thread(target=sending, name="send")
+    rcv.start()
+    send.start()
+    # Wait for both threads to terminate
+    rcv.join()
+    send.join()
