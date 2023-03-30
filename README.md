548b200578d4e80be7cf5dc6729f0e4de9319873
AHP-23 <alexchp@bu.edu>
2023-03-30 10:31:04 -0400

    P2P

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ peer to peer chat
 Every client is an independent network 
 All clients connect to a SQL database.
 All chats are locally stored.
+
+## TODO
+- User can only receive or send
+- User can only send one message before code ends
+- Check between two computers
+- Add database
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+#cd ..\..\Users\acmhp\Desktop\EC530\Assignment\P2P_Chat\
 from socket import *
 
-User1_Port = 2907
-User2_Port = 2908
+Port = 2909
 User1_IP_addr = gethostbyname(gethostname())
 User2Socket = socket(AF_INET, SOCK_STREAM)
 User1Socket = socket(AF_INET, SOCK_STREAM)
 
-def main():
-    User1Socket.bind((User1_IP_addr, User1_Port))
+def receiving():
+    User1Socket.bind((User1_IP_addr, Port))
     User1Socket.listen(1)
 
     print("Waiting for connection...")
@@ -25,10 +25,22 @@ def main():
 
 
 def sending():
-    User2_IP_addr = input("Enter IP address of User2: ")
+    #User2_IP_addr = input("Enter IP address of User2: ")
     User2_IP_addr = gethostbyname(gethostname())
 
+    User2Socket.connect((User2_IP_addr, Port))
+
+    # Send message
+    message = input("Enter message to send: ")
+    User2Socket.send(message.encode())
 
 
 if __name__ == "__main__":
-    main()
+    # Prompt user to send or receive messages
+    choice = input("Enter 'send' or 'receive': ")
+    if choice == "send":
+        sending()
+    elif choice == "receive":
+        receiving()
+    else:
+        print("Invalid choice")
